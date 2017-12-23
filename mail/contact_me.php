4a00b648e75238f0a163a0d8f2730930ebf2ded5
Lukas Schmidtpeter <lukas@infoschmidtpeter.de>
2017-12-23 12:39:15 +0100

    tt

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,143 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'blackhorsesraitersaich@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: blackhorsesraitersaich@yahoo.com "; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+
+
+$von = $_POST['von'];
+
+  $email = $_POST['email'];
+
+  $betreff = $_POST['betreff'];
+
+  $nachricht = $_POST['nachricht'];
+
+  $fertig = $_POST['fertig']; 
+
+
+
+function checkEmail( $email )
+
+  {
+
+  $nonascii      = "x80-xff"; 
+
+  $nqtext        = "[^\$nonascii1512"]";
+
+  $qchar         = "\[^$nonascii]";
+
+  $normuser      = '[a-zA-Z0-9][a-zA-Z0-9_.-]*';
+
+  $quotedstring  = ""(?:$nqtext|$qchar)+"";
+
+  $user_part     = "(?:$normuser|$quotedstring)";
+
+  $dom_mainpart  = '[a-zA-Z0-9][a-zA-Z0-9._-]*.';
+
+  $dom_subpart   = '(?:[a-zA-Z0-9][a-zA-Z0-9._-]*.)*';
+
+  $dom_tldpart   = '[a-zA-Z]{2,5}';
+
+  $domain_part   = "$dom_subpart$dom_mainpart$dom_tldpart";
+
+  $pattern       = "$user_part@$domain_part";
+
+  
+
+  if (!preg_match( "/$pattern$/", $email ))
+
+  {
+
+  return FALSE;
+
+  }
+
+  else
+
+  {
+
+  return TRUE;
+
+  }
+
+  } 
+
+
+if(isset($fertig)){
+
+
+ if ($von == "") {
+
+  echo"<script type="text/javascript"> alert("Sie haben ihren Namen nicht angegeben!");</script>";
+
+  } elseif ($email == "") {
+
+  echo"<script type="text/javascript"> alert("Sie haben ihre Email nicht angegeben!");</script>";
+
+  } elseif ($betreff == "") {
+
+  echo"<script type="text/javascript"> alert("Sie haben keinen Betreff angegeben!");</script>";
+
+  } elseif ($nachricht == "") {
+
+  echo"<script type="text/javascript"> alert("Sie haben keine Nachricht angegeben!");</script>"; 
+
+  } elseif (!checkEmail( $_REQUEST['email'] )) {
+
+  echo"<script type="text/javascript"> alert("Die Email Adresse ist nicht gueltig!");</script>";
+
+  } else {
+
+  
+
+  
+
+
+
+$datum = date("d.m.Y");
+
+  $uhrzeit = date("H:i");
+
+  $datum=$datum ."-". $uhrzeit ."Uhr";
+
+
+
+ 
+
+  $ich = "blackhorsesraitersaich@yahoo.com";
+
+  $betreffemail = "Kontaktformular";
+
+  $text = "Nachricht von: ".$von."
+
+  Seine e-Mail Adresse:  ".$email."
+
+  Betreff:  ".$betreff."
+
+  Sendedatum:  ".$datum."
+
+  Nachticht:  ".$nachricht."
+
+  
+
+  
+
+  
+
+  ";
+
+  mail($ich, $betreffemail, $text, 
+
+  "From: Mustername <Absenderemail>");
+
+  
+
+  echo"<script type="text/javascript"> alert("Die Nachricht wurde erfolgreich verschickt!");</script>"; 
+
+
+ 
+
+
+}
+
+  }
+
+  ?> 
+
